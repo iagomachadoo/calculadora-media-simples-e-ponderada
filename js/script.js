9106c3b93a9a01b1e968dd1eeb202adc21b83182
Iago Machado <83992018+iagomachadoo@users.noreply.github.com>
2021-12-14 19:06:20 -0300

    Adicionando javascript
    Pegando os elementos do primeiro bloco via javascript.

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let numeroDeAvaliacoes = document.querySelector('#avaliacao')
+
+let mediaSimplesOuPonderada = document.getElementsByName('media-simples-ponderada')
+
+let mediaMinimaDeAprovacao = document.querySelector('#aprovacao')
+
+let mediaMinimaDeRecuperacao = document.querySelector('#recuperacao')
+
+let btnInserirNotas = document.querySelector('#inserir-notas')
+
+function inserirNotas(){
+    console.log("Olá, Mundo!");
+}
+
+function somarNotas(){
+
+}
+
+/*
+console.log(numeroDeAvaliacoes);
+console.log(mediaSimplesOuPonderada);
+console.log(mediaMinimaDeAprovacao);
+console.log(mediaMinimaDeRecuperacao);
+console.log(btnInserirNotas);
+*/
